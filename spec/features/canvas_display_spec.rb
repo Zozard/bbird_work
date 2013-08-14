f90a8fb29475d2ad567d139ca54e13d43ee62f62
Zoz <g.zozine@gmail.com>
2013-08-14 19:00:03 +0200

    Capybara tests for canvas green

--- spec/features/canvas_display_spec.rb
+++ spec/features/canvas_display_spec.rb
@@ -1,28 +1,36 @@
 require 'spec_helper'
 
-describe "canvas" do
+feature "canvas" do
 
-  before(:each) do
-    u=User.create(email: 'user@example.com', password: 'password', name: 'denis')
-    login_as(u, :scope => :user)
+  background do
+    u=User.new(email: 'user@example.com', password: 'password', name: 'denis')
     @c=Canvas.create(title: "Noisy Keychains", user: u)
+    login_as(u, :scope => :user)
     visit '/canvas/'+@c.id.to_s
   end
 
-  it "each 9 <td> contains respectively contents of 9 blocks" do
+  after(:each) do
+    Warden.test_reset!
+  end
+
+  scenario "each 9 <td> contains respectively contents of 9 blocks" do
     all("td").each_with_index do |td, index|
       td.should have_content(@c.content_of_block(index+1))
     end
   end
 
-  it "textareas make changes in database", :js => true do
-    all("td").each_with_index do |td, index|
-      within("form#block_"+(index+1).to_s) do
-        fill_in 'block_content', :with => '123456789'+index.to_s
-      end
-      #page.driver.execute_script("document.getElementById('block_'#{(index+1).to_s}).submit();")
-      @c.content_of_block(index+1).should have_content('123456789'+index.to_s)
+  scenario "if js active, submit button is hidden", :js => true do
+    within('form#block_1') do
+      # Par défaut Capybara ne tient pas compte des éléments invisibles
+      find_button('Submit', :visible => false).should_not be_visible
     end
   end
 
+      scenario "textareas make changes in database for the first block" do
+        within("form#block_1") do
+          fill_in 'block_content', :with => '123456789'
+          click_on 'Submit'
+        end
+        @c.content_of_block(1).should have_content('123456789')
+      end
 end
