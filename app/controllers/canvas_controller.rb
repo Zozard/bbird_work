dd40868165628eff48bef951822739351494c118
Zoz <g.zozine@gmail.com>
2013-08-20 10:52:57 +0200

    Icons on the view

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -4,6 +4,7 @@ class CanvasController < ApplicationController
   
   def show
     @blocks=Block.where(canvas_id: params[:id])
+    @id_to_name = {1=>"KP", 2=>"KA", 3=>"VP", 4=>"CR", 5=>"CS", 6=>"KR", 7=>"CH", 8=>"C$", 9=>"RS"} 
   end
 
   def index
